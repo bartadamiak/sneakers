cc71fe0353b6c30ee8f6d0f67f8f23c1a4d8521b
bartadamiak <bart.adamiak@gmail.com>
2022-09-10 12:49:13 +0200

    cart

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
 const mainPhotos = document.querySelector('.main-photos');
 const smallPhotos = document.querySelectorAll('.small-photos img');
 
+const number = document.querySelector('.number');
+const amountBtn = document.querySelectorAll('.amount-btn');
+
+const cartBtn = document.querySelector('.cart-btn');
+
+const cart = document.querySelector('.cart')
+
+const remove = document.querySelector('.remove-icon');
+
+const purchaseAmount = document.querySelector('.purchase-amount');
+
+const finalPrice = document.querySelector('.final-price');
+
 smallPhotos.forEach(element => {
     element.addEventListener('click', function (e) {
         e.preventDefault();
@@ -29,3 +42,33 @@ smallPhotos.forEach(element => {
 
     })
 });
+
+
+let shoesNumber = number.innerHTML;
+shoesNumber = parseInt(shoesNumber);
+
+
+amountBtn.forEach(element => {
+    element.addEventListener('click', function(e) {
+        
+        if(element.classList.contains('plus')) {
+            shoesNumber++;
+            number.innerHTML = shoesNumber;
+            purchaseAmount.innerHTML = shoesNumber
+        }
+
+        if(element.classList.contains('minus') && shoesNumber != 0) {
+            shoesNumber--;
+            number.innerHTML = shoesNumber;
+            purchaseAmount.innerHTML = shoesNumber
+        }
+    })
+});
+
+cartBtn.addEventListener('click', function() {
+    cart.classList.remove('hide');
+})
+
+remove.addEventListener('click', function() {
+    cart.classList.add('hide');
+})
